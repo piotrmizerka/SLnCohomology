fad833e753c1b63b2109511bc664d3679aed07ab
piotrmizerka <piotrmizerka@gmail.com>
2023-07-01 01:34:59 +0200

    representation as slnz elts fixed

--- SLnCohomology/src/helpful_functions.jl
+++ SLnCohomology/src/helpful_functions.jl
@@ -19,12 +19,13 @@ end
 # We apply Gaussian elimination and Euclidean algorithm.
 function gelt_from_matrix(M::AbstractMatrix, sln)
     inv_result, temp_matrix = one(sln), copy(M)
+    N = size(first(gens(sln)))[1]
     for i in 1:(N-1)
         factor, temp_matrix = reduce_column_down(i,temp_matrix, sln)
         inv_result = factor*inv_result
     end
     for i in N:-1:2
-        factor, temp_matrix = reduce_column_up(i,temp_matrix, slnp_order)
+        factor, temp_matrix = reduce_column_up(i,temp_matrix, sln)
         inv_result = factor*inv_result
     end
     result = inv_result^(-1)
@@ -41,6 +42,7 @@ function reduce_column_up(col::Integer, M::AbstractMatrix, sln)
 end
 function reduce_column_down(col::Integer, M::AbstractMatrix, sln)
     result_factor, result_matrix = one(sln), copy(M)
+    N = size(first(gens(sln)))[1]
     for k in (col+1):N
         factor, result_matrix = euclidean_algorithm(col,k,col,result_matrix,sln)
         result_factor = factor*result_factor
@@ -74,6 +76,7 @@ function euclidean_algorithm(i_row::Integer, j_row::Integer, col::Integer, M::Ab
 end
 function add_row_multiplicity(i::Integer, j::Integer, mult::Integer, M::AbstractMatrix, sln)
     E(i,j) = E_(i,j,sln)
+    N = size(first(gens(sln)))[1]
     if mult != -1 && mult != 1
         result_factor = E(i,j)^mult
     elseif mult == 1
@@ -90,6 +93,7 @@ end
 function flip_two_rows(i::Integer, j::Integer, M::AbstractMatrix, sln)
     E(i,j) = E_(i,j,sln)
     result_factor, result_matrix = E(i,j)*E(j,i)^(-1)*E(i,j), copy(M)
+    N = size(first(gens(sln)))[1]
     for k in 1:N
         result_matrix[i,k] = M[j,k]
         result_matrix[j,k] = -M[i,k]
@@ -99,6 +103,7 @@ end
 function invert_two_rows(i::Integer, j::Integer, M::AbstractMatrix, sln)
     E(i,j) = E_(i,j,sln)
     result_factor, result_matrix = E(i,j)^2*E(j,i)^(-1)*E(i,j)^2*E(j,i)^(-1), copy(M)
+    N = size(first(gens(sln)))[1]
     for k in 1:N
         result_matrix[i,k] = -M[i,k]
         result_matrix[j,k] = -M[j,k]
@@ -106,6 +111,7 @@ function invert_two_rows(i::Integer, j::Integer, M::AbstractMatrix, sln)
     return result_factor, result_matrix
 end
 function E_(i::Integer, j::Integer, sln)
+    N = size(first(gens(sln)))[1]
     Eij = MatrixGroups.ElementaryMatrix{N}(i,j,Int8(1))
     for s in gens(sln)
         if MatrixGroups.matrix_repr(s) == MatrixGroups.matrix_repr(Eij)

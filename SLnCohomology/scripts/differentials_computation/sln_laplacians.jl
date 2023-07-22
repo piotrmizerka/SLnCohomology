66898581e54c29971edb7a5ba8e23eff1bd90030
piotrmizerka <piotrmizerka@gmail.com>
2023-07-22 19:36:19 +0200

    differentials computation for any sln
    TODO: check indices!

---
+++ SLnCohomology/scripts/differentials_computation/sln_laplacians.jl
@@ -0,0 +1,55 @@
+# Let's load the precompuded differential data (can take about 24hours ???)
+include("sln_utils.jl");
+
+# Sanity checks for vanishing compositions of differentials
+# we can do sanity check for consecutive degrees only
+consecutive_degrees = []
+for i in 1:(length(homology_degrees)-1)
+    if homology_degrees[i+1] == homology_degrees[i]+1
+        push!(consecutive_degrees,(homology_degrees[i],homology_degrees[i+1]))
+    end
+end
+for pair in consecutive_degrees
+    n = pair[2]+1
+    d_n_minus_1 = LowCohomologySOS.embed.(identity, d[n-1], Ref(RG_Δ[n-1]))
+    d_n = LowCohomologySOS.embed.(identity, d[n], Ref(RG_Δ[n-1]))
+    @assert d_n_minus_1*d_n == [zero(RG_Δ[n-1]) for i in 1:cells_number[n-2],j in 1:cells_number[n]]
+end
+
+# The stabiliser parts which we have to add to get free modules.
+# Hopefully the stabilisers' elements belong to the half_bases.
+stab_part_dim = Dict()
+for n in homology_degrees
+    k_n = cells_number[n]
+    stab_part_dim[n] = [
+        i == j ? one(RG_Δ[n])-SLnCohomology.averaged_rep(m_stabs[n][i],RG_Δ[n]) : zero(zero(RG_Δ[n])) 
+        for i in 1:k_n,j in 1:k_n
+    ]
+end
+
+# Compute the Laplacians (time consuming, can take about ??).
+# At the end, we embed the Laplacian into RG_star, the group ring
+# with the same basis as RG but with twisted multiplciation, i.e.
+# (1+g)(1+h)=1+g+h+g^(-1)h. This is needed to translate hermitian squares
+# to the standard ones for the definition of the semi-definite optimization problem
+# (solvers prefer standard squares to hermitian :).
+# This has no effect on the shape of the Laplacian since we just embed it.
+RG_Δ_star = Dict()
+for n in homology_degrees
+    RG_Δ_star[n] = LowCohomologySOS.group_ring(sln, half_basis_Δ[n], star_multiplication = true)
+end
+# We may miss extreme Laplacians which require only one differential to be defined.
+# One can consider them separately instead.
+Δ = Dict()
+for pair in consecutive_degrees
+    n = pair[1]+1
+    d_n = LowCohomologySOS.embed.(identity, d[n], Ref(RG_Δ[n]))
+    d_n_plus_1 = LowCohomologySOS.embed.(identity, d[n+1], Ref(RG_Δ[n]))
+    Δ[n-1] = d_n'*d_n+d_n_plus_1*d_n_plus_1'+stab_part_dim[n]
+    Δ[n-1] = LowCohomologySOS.embed.(identity, Δ[n-1], Ref(RG_Δ_star[n]))
+end
+# A sanity check: check if the Laplacians are hermitian.
+for pair in consecutive_degrees
+    n = pair[1]
+    @assert Δ[n]' == Δ[n]
+end
--- SLnCohomology/scripts/differentials_computation/sln_utils.jl
+++ SLnCohomology/scripts/differentials_computation/sln_utils.jl
@@ -7,23 +7,32 @@ using LinearAlgebra
 using Serialization
 using SLnCohomology
 
-const N = 5
+const N = 3 # hard-coded, to change if one wants other slns
 sln = MatrixGroups.SpecialLinearGroup{N}(Int8)
 sln_gens = gens(sln)
 
-# Load the data from Ben
-sl5_bound_stab = deserialize(joinpath(@__DIR__, "./sl5_bound_stab.sjl"))
+# Load the data from Ben. As for now, hard-coded - if different sln is to be loaded,
+# change the path to the appropriate "./sln_bound_stab.sjl" file
+sln_bound_stab = deserialize(joinpath(@__DIR__, "./sl3_bound_stab.sjl"))
 
 # Extract homology degrees
-differential_degrees = sort([first(x) for x in sl5_bound_stab["boundaries"]])
+differential_degrees = sort([first(x) for x in sln_bound_stab["boundaries"]])
+
+# Compute the stabilisers as signed subgroups of SL(n,ℤ)
+m_stabs = Dict()
+for n in differential_degrees
+    m_stabs[n] = [
+        [(SLnCohomology.gelt_from_matrix(M,sln),σ) for (M,σ) in stab] for stab in sln_bound_stab["stabilisers"][n]
+    ]
+end
 
 # Compute the supports (i.e. half_bases) for the group rings to compute the Laplacians
 # - we just add to half_basis the coset elements appearing in the differentials.
 d_union = Dict(k=>[one(sln)] for k in differential_degrees)
 for k in differential_degrees
-    for i in eachindex(sl5_bound_stab["boundaries"][k])
-        for j in eachindex(sl5_bound_stab["boundaries"][k][i])
-            coset = sl5_bound_stab["boundaries"][k][i][j]["orbit_coset_with_orientation"]
+    for i in eachindex(sln_bound_stab["boundaries"][k])
+        for j in eachindex(sln_bound_stab["boundaries"][k][i])
+            coset = sln_bound_stab["boundaries"][k][i][j]["orbit_coset_with_orientation"]
             d_union[k] = union(
                 d_union[k], 
                 [SLnCohomology.gelt_from_matrix(M,sln) for (M,σ) in coset]
@@ -33,11 +42,11 @@ for k in differential_degrees
 end
 half_basis_Δ = Dict()
 min_degree = differential_degrees[1]
-half_basis_Δ[min_degree-1] = d_union[min_degree]
-half_basis_Δ[min_degree-1] = unique(
-    [half_basis_Δ[min_degree-1];inv.(half_basis_Δ[min_degree-1])]
+half_basis_Δ[min_degree] = d_union[min_degree+1]
+half_basis_Δ[min_degree] = unique(
+    [half_basis_Δ[min_degree];inv.(half_basis_Δ[min_degree])]
 )
-for k in differential_degrees[2:end]
+for k in differential_degrees[3:end]
     half_basis_Δ[k-1] = union(d_union[k-1], d_union[k])
     half_basis_Δ[k-1] = unique([half_basis_Δ[k-1];inv.(half_basis_Δ[k-1])])
 end
@@ -48,12 +57,11 @@ end
 # cohomologies of degree: 1, 2, 3, 4, and 5, of which 4 and 5 remain interesting
 # as we have vanishing for 1, 2, 3 from Bader-Sauer.
 homology_degrees = []
-for k in differential_degrees
-    if length(half_basis_Δ[k-1]) <= 11_500
-        push!(homology_degrees,k-1)
+for k in differential_degrees[1:end-1]
+    if length(half_basis_Δ[k]) <= 11_500
+        push!(homology_degrees,k)
     end
 end
-homology_degrees
 RG_Δ = Dict()
 for k in homology_degrees
     RG_Δ[k] = LowCohomologySOS.group_ring(
@@ -61,20 +69,19 @@ for k in homology_degrees
     )
 end
 
-# TODOO!!!!!! FROM THIS POINT NOT VERIFIED - STH WITH hom indices wrong!
 # Compute the differential as matrices over RGs
-cells_number = Dict(k=>length(sl5_bound_stab["stabilisers"][k]) for k in homology_degrees)
-dx = Dict([(k,i,j) => 0//1*zero(RG_Δ[k-1]) for k in 4:8 for i in 1:cells_number[k-1] for j in 1:cells_number[k]])
+cells_number = Dict(k=>length(sln_bound_stab["stabilisers"][k]) for k in differential_degrees)
+dx = Dict([(k+1,i,j) => 0//1*zero(RG_Δ[k]) for k in homology_degrees for i in 1:cells_number[k] for j in 1:cells_number[k+1]])
 for k in homology_degrees
-    for j in eachindex(sl5_bound_stab["boundaries"][k])
-        for summand in sl5_bound_stab["boundaries"][k][j]
+    for j in eachindex(sln_bound_stab["boundaries"][k+1])
+        for summand in sln_bound_stab["boundaries"][k+1][j]
             coset_orient = [(SLnCohomology.gelt_from_matrix(M,sln),σ) for (M,σ) in summand["orbit_coset_with_orientation"]]
-            summand_in_RG = summand["sign"]*averaged_rep(coset_orient,RG_Δ[k-1])
-            dx[k,summand["orbit_standard_cell"],j] += summand_in_RG
+            summand_in_RG = summand["sign"]*SLnCohomology.averaged_rep(coset_orient,RG_Δ[k])
+            dx[k+1,summand["orbit_standard_cell"],j] += summand_in_RG
         end
     end
 end
 d = Dict()
 for k in homology_degrees
-    d[k] = [dx[k+1,i,j] for i in 1:cells_number[k], j in 1:cells_number[k+1]]
+    d[k+1] = [dx[k+1,i,j] for i in 1:cells_number[k], j in 1:cells_number[k+1]]
 end

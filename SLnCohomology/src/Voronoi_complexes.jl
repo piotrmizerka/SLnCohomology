171b419ceab1fc4903952e7252f3a4659f9dd1b6
benjaminbrueck <benjamin.brueck.maths@gmail.com>
2024-01-22 15:55:46 +0100

    Used different library for polyhedra and added check for even n

--- SLnCohomology/src/Voronoi_complexes.jl
+++ SLnCohomology/src/Voronoi_complexes.jl
@@ -1,12 +1,16 @@
-#Pkg.add("Polyhedra")
 using Polyhedra
+import GLPK
 
 function create_polyhedron(cell)
+    #= cell is a matrix whose columns are the minimal vectors spanning the corresponding polyhedron
+    creates a polyhedron in the polyhedra package
+    =#
+    lib = DefaultLibrary{Float64}(GLPK.Optimizer) # one could set different libraries here
     poly_vertices = Vector{Int64}[]
     for col in eachcol(cell)
         push!(poly_vertices,vec(SLnCohomology.quadratic_form(col)))
     end
-    poly_cell = polyhedron(vrep(poly_vertices))
+    poly_cell = polyhedron(vrep(poly_vertices),lib) 
     return poly_cell
 end
 
@@ -20,6 +24,7 @@ function facets(polyhedral_cell, min_vectors, codim_1_cells)
         vertices_facet = []
         for vertex in incidentpoints(polyhedral_cell, halfspace) # read out using Oscar/polymake in the long term, this might get shorter
             # also careful: At the moment, I'm not reducing the cells, this should be done or avoided by using polymake
+            # But it's checked that the results agree with what was done before
             vertex_index = findfirst(x -> x==vertex, vertex_list) # this should somehow get quicker, but I couldn't find out how
             push!(vertices_facet,min_vectors[vertex_index])
         end
@@ -35,31 +40,44 @@ function facets(polyhedral_cell, min_vectors, codim_1_cells)
     return codim_1_cells
 end
 
-function min_vectors_facet(cell,codim_1_cells)
-    min_vectors = collect(eachcol(cell))
-    return facets(create_polyhedron(cell), min_vectors, codim_1_cells)
+function facets_min_vectors_cell(cell,codim_1_cells)
+    #= cell is a matrix whose columns are the minimal vectors spanning the corresponding polyhedron
+    computes the corresponding polyhedral cell and extracts the minimal vectors in a list
+    =#
+    return facets(create_polyhedron(cell), collect(eachcol(cell)), codim_1_cells)
 end
 
-#=
-STILL TBD: The sign flip for even n Implement in the following.
-flip_matrix = [-1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]
+function flip_matrix(n)
+    flip = Matrix(1I, n, n)
+    flip[1] = -1
+    return flip
+end
 
-for (dimension,cells) in cells_GL4
-    for cell in cells
-        SL_orbit = flip_matrix*cell
-        # check whether its in the orbit of one of the other cells
-        if ! orbit_in_list(SL_orbit,cells_SL4[dimension])
-            push!(cells_SL4[dimension],SL_orbit)
-        end         
+function add_sl_n_orbits(list_of_forms)
+    #= Takes a list of forms, all of the same dimension such that they lie in distinct GL_n orbits. 
+    Adds their SL_n orbits ot the list.
+    =#
+    dim = size(list_of_forms[1])[1]
+    flip = flip_matrix(dim)
+    if iseven(dim)
+        for form in list_of_forms
+            SL_orbit = flip*form
+            # check whether its in the orbit of one of the other cells
+            if ! orbit_in_list(SL_orbit,list_of_forms)
+                push!(list_of_forms,SL_orbit)
+            end   
+        end        
     end
+    # if dim is odd, GL and SL orbits are the same, so nothing tbd
+    return list_of_forms
 end
-=#
 
 function Voronoi_cells(n,perfect_forms)
     #= perfect_forms: list of perfect forms in dimension n, up to action of GL
     =#
     dim_symmetric_space = n*(n+1)/2-1
     cells_SLn = Dict()
+    perfect_forms = add_sl_n_orbits(perfect_forms)
     perfect_forms_min_vec_rep = []
     for perfect_form in perfect_forms
         # compute the minimal vectors and put them in the right format
@@ -71,7 +89,7 @@ function Voronoi_cells(n,perfect_forms)
         println("I'm computing in dimension $dim")
         cells_SLn[dim] = []
         for cell in cells_SLn[dim+1]
-            min_vectors_facet(cell,cells_SLn[dim]) # function needs better name
+            facets_min_vectors_cell(cell,cells_SLn[dim]) # function needs better name
         end
     end
     return cells_SLn
